3fc50b496e19330f123048a4e97e708085dec376
felipe <felipedayoub@usp.br>
2026-07-16 20:58:35 -0300

    Atualiza rotas da API e script para construção da base de dados

--- backend/app/tmdb.py
+++ backend/app/tmdb.py
@@ -47,15 +47,15 @@ def createFulltextIndex():
 
 def selectPage(page: int, language: str) -> str:
     url_pt = f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR&page={page}&sort_by=vote_average.desc&vote_average.gte=6.4&vote_count.gte=50&with_origin_country=BR&with_original_language=pt"
-    url_en = f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR&page={page}&sort_by=vote_average.desc&vote_average.gte=6.9&vote_count.gte=4000&with_original_language=en"    
+    url_en = f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR&page={page}&sort_by=vote_average.desc&vote_average.gte=6.0&vote_count.gte=4000&with_original_language=en"    
     if language == "en":
         return url_en
     else:
         return url_pt
 
-def moviePages(P: int, language: str):
+def moviePages(totalPages: int, language: str):
     page = 1
-    while page <= P:
+    while page <= totalPages:
         url = selectPage(page, language)
         addMovies(url)
         page+=1
@@ -73,13 +73,15 @@ def addMovies(movieURL: str):
                 releaseDate = movie["release_date"] 
                 voteAverage = movie["vote_average"]
                 genreIds = movie["genre_ids"]
+                popularity = movie["popularity"]
                 query = """
                 MERGE (m:Movie {tmdbId:$tmdbId}) ON CREATE SET m.title=$title, m.overview=$overview,
                 m.posterPath=$posterPath, m.releaseDate=$releaseDate, m.voteAverage=$voteAverage,
-                m.genreIds=$genreIds   
+                m.genreIds=$genreIds, m.popularity=$popularity   
                 """
                 session.run(query, title=title, tmdbId=tmdbId, overview=overview, posterPath=posterPath,
-                            genreIds=genreIds, releaseDate=releaseDate, voteAverage=voteAverage)
+                            genreIds=genreIds, releaseDate=releaseDate, voteAverage=voteAverage, 
+                            popularity=popularity)
                 moviesID.append(tmdbId)
                 print(f"Movie added: {title} (id: {tmdbId})")
     else:
@@ -104,7 +106,7 @@ def addDirector(name, tmdbId, profilePath, movieID):
         """
         session.run(query, movieID=movieID, tmdbId=tmdbId, name=name, profilePath=profilePath)
 
-def credits(N: int):
+def credits(castSize: int):
     for movieID in moviesID:
         creditsURL = f"https://api.themoviedb.org/3/movie/{movieID}/credits?language=pt-BR"
         response = requests.get(creditsURL, headers=headers)
@@ -115,7 +117,7 @@ def credits(N: int):
                 for actor in data["cast"]:
                     addActor(actor["name"], actor["character"], actor["id"], actor["profile_path"], movieID)  
                     i+=1
-                    if i>N: # Add only the first N actors  
+                    if i>castSize: # Add only the first `castSize` actors  
                         break
                 for member in data["crew"]: # Add directors 
                     if member.get("job") == "Director":
@@ -124,23 +126,81 @@ def credits(N: int):
             print(f"Cast added (id: {movieID})")
         else:
             print(f"ERRO: {response.status_code}")
-        
-            
 
-def execute(deleteDB: bool = True, createMovies: bool = True, createArtists: bool = True):
+def getPeopleId() -> list:
+    IDs = []
+    with driver.session(database=DATABASE_NAME) as session:
+        query = """
+        MATCH (p:Person)
+        RETURN p.tmdbId as tmdbId
+        """
+        records = session.run(query)
+        for person in records:
+            IDs.append(person["tmdbId"])
+    return IDs
+
+
+#def addBio(id: int):
+#    actorURL = f"https://api.themoviedb.org/3/person/{id}?language=pt-BR"
+#    response = requests.get(actorURL, headers=headers)
+#    if response.status_code == 200:
+#        data = response.json()
+
+def addArtistDetails(artist: dict):
+    with driver.session(database=DATABASE_NAME) as session:
+        query = """
+        MATCH (p:Person {tmdbId: $tmdbId}) 
+        SET p.biography = $biography, p.birthday = $birthday, p.deathday = $deathday,
+            p.homepage = $homepage, p.knownFor = $knownFor, p.placeOfBirth = $placeOfBirth,
+            p.popularity = $popularity,
+            p.degree = COUNT {
+            MATCH (p)-[:ACTED_IN|DIRECTED]->(:Movie)
+            }
+        """
+        session.run(query, tmdbId=artist["tmdbId"], biography=artist["biography"], birthday=artist["birthday"],
+                    deathday=artist["deathday"], homepage=artist["homepage"], knownFor=artist["knownFor"],
+                    placeOfBirth=artist["placeOfBirth"], popularity=artist["popularity"])
+        print(artist["name"], "added")
+
+
+def peopleDetails():
+    IDs = getPeopleId()
+    for id in IDs:
+        artistURL = f"https://api.themoviedb.org/3/person/{id}?language=pt-BR"
+        response = requests.get(artistURL, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            artist = {"tmdbId": id}
+            artist["biography"] = data["biography"]
+            artist["birthday"] = data["birthday"]
+            artist["deathday"] = data["deathday"] # null if alive
+            artist["homepage"] = data["homepage"]
+            artist["knownFor"] = data["known_for_department"]
+            artist["placeOfBirth"] = data["place_of_birth"]
+            artist["popularity"] = data["popularity"]
+            artist["name"] = data["name"]
+            addArtistDetails(artist)
+        else:
+            print(f"ERRO: {response.status_code}")
+    
+
+def execute(deleteDB: bool = True, createMovies: bool = True, createArtists: bool = True, createDetails: bool = False):
     if deleteDB:
         delete()
         createConstraints()
         createFulltextIndex()
     if createMovies:
-        moviePages(P=39, language="en")
+        moviePages(totalPages=63, language="en")
         if createArtists:
-            credits(N=5)
+            credits(castSize=4)
+    if createDetails:
+        peopleDetails()
+    
 
 
 def main():
     try: 
-        execute()
+        execute(deleteDB=False, createMovies=False, createDetails=True)
     except Exception as e:
         print(f"Error: {e}")
         raise
